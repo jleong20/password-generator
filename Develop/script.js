2339759f8205f530f0396c5a93927c78bd5752e9
Jerry Leong <jerry.leong20@gmail.com>
2020-09-19 19:00:49 -0700

    add prompts to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//add var
+var pwLength;
+var pwLower;
+var pwUpper;
+var pwNum;
+var pwSpec;
+var password;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +16,18 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+//add display funct.
+function generatePassword(){
+  //add prompts
+  pwLength = prompt("length of pw");
+  pwLower = prompt("lowercase?");
+  pwUpper = prompt("upper?");
+  pwNum = prompt("numbers?");
+  pwSpec = prompt("special chars?");
+
+  password = "hello";
+  return password;
 }
 
 // Add event listener to generate button
